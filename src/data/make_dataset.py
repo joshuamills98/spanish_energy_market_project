89086ca6172f1643341229bf4cffddd12c746fe3
joshuamills98 <57185163+joshuamills98@users.noreply.github.com>
2021-07-14 15:37:32 +1000

    continued time series analysis

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 import pandas as pd
 
+def resample_dataframe(df):
+    df = df.resample('1d').mean()
+    return df
 
 def drop_absent_cols(df):
     df = df.fillna(0)
--- src/stattests.py
+++ src/stattests.py
@@ -1,5 +1,7 @@
 from statsmodels.tsa.stattools import adfuller
+from scipy.stats import normaltest
 import pandas as pd
+import os
 
 
 def adf_test(timeseries, name):
@@ -18,3 +20,28 @@ def adf_test(timeseries, name):
         dfoutput['Test Statistic'] < dfoutput['Critical Value (1%)'] else
         'Accept Null Hypothesis, Series is Non-Stationary')
     print('\n')
+
+
+def normal_test(timeseries, name, alpha=5e-3):
+    print('Results of D’Agostino and Pearson Test for {}:'.format(name))
+    k2, p = normaltest(timeseries, axis=0, nan_policy='omit')
+    print("p = {:g}".format(p))
+    print("null hyp: series comes from a normal distribution")
+    if p < alpha:  # null hypothesis: x comes from a normal distribution
+        print("The null hypothesis can be rejected")
+    else:
+        print("The null hypothesis cannot be rejected")
+
+
+def lag_correlation_test(timeseries, name, lag=1):
+    values = pd.DataFrame(timeseries.values)
+    dataframe = pd.concat([values.shift(lag), values], axis=1)
+    dataframe.columns = ['t-1', 't+1']
+    result = dataframe.corr()
+    print("Lag Correlation for {} = {}".format(name, result))
+
+
+if __name__ == '__main__':
+    path = os.path.join('.', 'data', 'raw', 'energy_dataset.csv')
+    df = pd.read_csv(path)
+    normal_test(df.loc[:, 'generation fossil oil'], 'generation biomass')
